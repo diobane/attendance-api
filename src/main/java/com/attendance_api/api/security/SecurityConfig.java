a3c75a5e83917ebdd7b40b58e7b53f476cbe02f6
Giovanny <giolivaze@gmail.com>
2026-06-10 21:32:13 -0300

    Fixed swagger documentation

--- src/main/java/com/attendance_api/api/security/SecurityConfig.java
+++ src/main/java/com/attendance_api/api/security/SecurityConfig.java
@@ -40,10 +40,10 @@ public class SecurityConfig {
                         // Public
                         .requestMatchers(
                                 "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/webjars/**",
                                 "/v3/api-docs/**",
-                                "/v3/api-docs",
-                                "/swagger-ui/index.html",
-                                "/v3/api-docs/swagger-config"
+                                "/v3/api-docs"
                         ).permitAll()
                         .requestMatchers("/auth/user/login").permitAll()
                         .requestMatchers("/family").permitAll()
